4b784a16b7a0d7bd5fe229cfb8074440c8065dd0
Jacog00135 <2428207444@qq.com>
2025-04-17 18:03:26 +0800

    解决初始化时无法创建管理员用户的问题

--- manage.py
+++ manage.py
@@ -2,6 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.conf import settings
+
+
+def create_admin_user():
+    """自动创建管理员"""
+    from django.contrib.auth.models import User
+
+    username = settings.L4D2_MOD_MANAGER_ADMIN_USERNAME
+    user = User.objects.filter(username=username)
+    if not user:
+        password = settings.L4D2_MOD_MANAGER_ADMIN_PASSWORD
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
 
 
 def main():
@@ -16,6 +29,9 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+    # 项目之后的初始化
+    create_admin_user()
 
 
 if __name__ == '__main__':
